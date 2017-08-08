610410e25060d7e7e356f344712975daaa5dcb19
Abraham Serafino <abraham.serafino@monsanto.com>
2017-08-08 15:54:57 -0500

    Fix install

--- nobs.js
+++ nobs.js
@@ -1,68 +1,64 @@
 #! /usr/bin/env node
 
-const { createReadStream, mkdirSync, statSync } = require('fs')
-const { join } = require('path')
-const { maybeInstall } = require('npm-install-global')
-const { readFileSync, writeFileSync } = require('jsonfile')
-const { shell } = require('execa')
-const { Extract } = require('unzip')
+const { createReadStream } = require('fs');
+const { join } = require('path');
+const { maybeInstall } = require('npm-install-global');
+const { readFileSync, writeFileSync } = require('jsonfile');
+const { shell } = require('execa');
+const { Extract } = require('unzip');
 
-const projectPath = process.argv[2] || process.cwd()
-const projectName = projectPath.split('/').pop()
+const projectPath = process.argv[2] || process.cwd();
+const projectName = projectPath.split('/').pop();
 
 function generateScaffold() {
   return new Promise(resolve => {
-    console.log('Generating scaffold...')
+    console.log('Generating scaffold...');
     createReadStream(join(__dirname, 'nobs.zip'))
       .pipe(Extract({ path: projectPath }))
       .on('close', () => {
-        console.log('done.')
         resolve()
-      })
+      });
   })
 }
 
 function installYarn() {
-  console.log('Installing yarn...')
+  console.log('Installing yarn...');
 
   return new Promise(resolve =>
     maybeInstall('yarn', error => {
       if (error) {
-        console.error(error)
-        process.exit()
+        console.error(error);
+        process.exit();
       }
 
-      console.log('done.')
-      resolve()
-    })
+      resolve();
+    });
   )
 }
 
 function configurePackage() {
   console.log('Configuring package...')
-  const package = readFileSync(join(process.cwd(), 'package.json'))
-  package.name = projectName
-  writeFileSync(join(process.cwd(), 'package.json'), package, { spaces: 2 })
-  console.log('done.')
+  const package = readFileSync(join(process.cwd(), 'package.json'));
+  package.name = projectName;
+  writeFileSync(join(process.cwd(), 'package.json'), package, { spaces: 2 });
 }
 
 async function installDependencies() {
-  console.log('Installing dependencies...')
-  const result = await shell('yarn install')
-  console.log(result.stdout)
-  console.log('done.')
+  console.log('Installing dependencies... (this may take a minute or two)');
+  const result = await shell('yarn install');
+  console.log(result.stdout);
 }
 
 const main = async () => {
   try {
-    await generateScaffold()
-    process.chdir(projectPath)
-    await installYarn()
-    configurePackage()
-    await installDependencies()
+    await generateScaffold();
+    process.chdir(projectPath);
+    await installYarn();
+    configurePackage();
+    await installDependencies();
   } catch (error) {
-    console.error(error.toString())
+    console.error(error.toString());
   }
 }
 
-main()
+main();
